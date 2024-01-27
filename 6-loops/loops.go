50bd3b90b9545eae5890b05b0f15596fcf6747e2
adminazhar <admin@azhark.com>
2024-01-27 11:08:24 +0530

    add for loop

--- 6-loops/loops.go
+++ 6-loops/loops.go
@@ -6,10 +6,25 @@ func main() {
 	dc := []string{"batman", "justice league", "wonder women"}
 	fmt.Println(dc)
 
-	// this might be confusing, but go uses for syntax for while loop also
+	// while loop - this might be confusing, but go uses for syntax for while loop also
 	i := 0
 	for i < 3 {
 		fmt.Printf("array value for index %v is %v \n", i, dc[i])
 		i++
+	}
+
+	// normal for loop, same as other languages
+	for i := 0; i < len(dc); i++ {
+		fmt.Printf("- The array value for index %v is %v \n", i, dc[i])
+	}
+
+	// for loop for array, using index,value pair, same as python, foreach in php
+	for index, value := range dc {
+		fmt.Printf("--- The array value for index %v is %v \n", index, value)
+	}
+
+	// if you dont want any unused variable like index or value, use underscore
+	for _, value := range dc {
+		fmt.Printf("------The array value is %v \n", value)
 	}
 }
