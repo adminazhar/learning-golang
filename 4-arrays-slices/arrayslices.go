cde44806a91a6a7b801ba4ae961f475495d444b7
adminazhar <admin@azhark.com>
2024-01-27 10:24:11 +0530

    add full syntax array

--- 4-arrays-slices/arrayslices.go
+++ 4-arrays-slices/arrayslices.go
@@ -1,5 +1,15 @@
-package make
+package main
+
+import "fmt"
 
 func main() {
+	fmt.Println("hello")
 
+	// full syntax with defined length
+	var arr1 [3]int = [3]int{10, 12, 14}
+	fmt.Println(arr1)
+
+	// full syntax with defined length
+	var arr2 [2]string = [2]string{"hello", "world"}
+	fmt.Println(arr2)
 }
