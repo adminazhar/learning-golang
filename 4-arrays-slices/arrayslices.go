46cfa836514b0e74a4254365d6ca5566c8c0daaa
adminazhar <admin@azhark.com>
2024-01-27 10:32:37 +0530

    add array operations

--- 4-arrays-slices/arrayslices.go
+++ 4-arrays-slices/arrayslices.go
@@ -12,4 +12,27 @@ func main() {
 	// full syntax with defined length
 	var arr2 [2]string = [2]string{"hello", "world"}
 	fmt.Println(arr2)
+
+	// infered
+	var arr3 = [4]int{45, 47, 49, 99}
+	fmt.Println(arr3)
+
+	// shorthand
+	arr4 := [2]string{"batman", "robin"}
+	fmt.Println(arr4)
+
+	// length is infered, compiler decides the length based on values
+	arr5 := []int{18, 19, 20}
+	fmt.Println(arr5)
+
+	commonarr := []string{"batman", "dark knight", "dark knight rises"}
+	// accessing elements
+	fmt.Println(commonarr[0])
+	fmt.Println(commonarr[1])
+	// updating element
+	commonarr[1] = "the dark knight"
+	fmt.Println(commonarr)
+	// count length
+	println(len(commonarr))
+
 }
