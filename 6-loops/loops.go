f5a3c6c2a80498a60104cc22e6f75b0567207858
adminazhar <admin@azhark.com>
2024-01-27 11:04:26 +0530

    add while loop

--- 6-loops/loops.go
+++ 6-loops/loops.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("yo")
+	dc := []string{"batman", "justice league", "wonder women"}
+	fmt.Println(dc)
+
+	// this might be confusing, but go uses for syntax for while loop also
+	i := 0
+	for i < 3 {
+		fmt.Printf("array value for index %v is %v \n", i, dc[i])
+		i++
+	}
 }
